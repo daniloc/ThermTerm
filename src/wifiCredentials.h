63fd6ff7f21b542e27948b42b22f2a9a5028f3ef
Danilo Campos <mail@danilocampos.com>
2023-07-24 23:04:26 -0400

    Add Wifi credentials file

---
+++ src/wifiCredentials.h
@@ -0,0 +1,7 @@
+#ifndef SRC_WIFICREDENTIALS
+#define SRC_WIFICREDENTIALS
+
+char ssid[] = "YOUR_SSID";          // your network SSID (name)
+char pass[] = "YOUR_SSID_PASSWORD"; // your network password (use for WPA, or use as key for WEP)
+
+#endif // SRC_WIFICREDENTIALS
